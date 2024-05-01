0e73c4d31e9dfb6cf50e6e2ff87df7c38e21c368
AaronGonzalez06 <aaron060812060812@gmail.com>
2024-05-01 10:55:50 +0200

    Mejorando cosas que no estan en jectpack compose

--- app/src/main/java/com/AG_AP/electroshop/screens/LoginScreen.kt
+++ app/src/main/java/com/AG_AP/electroshop/screens/LoginScreen.kt
@@ -10,14 +10,32 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TimeInput
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -29,12 +47,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.AG_AP.electroshop.R
@@ -44,6 +66,7 @@ import com.AG_AP.electroshop.viewModels.Routes
 /**
  * Method that shows the front view of the Login Screen TODO sacarlo todo a metodos extras
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginFrontView(
     viewModel: LoginViewModel = viewModel(),
@@ -107,8 +130,11 @@ fun LoginFrontView(
                             Column {
                                 OutlinedTextField(
                                     value = dataUiState.username,
-                                    onValueChange = { viewModel.usernameChange(it) },
-                                    label = { Text("Usuario") }
+                                    trailingIcon = { Icon(imageVector = Icons.Default.Person, contentDescription = "emailIcon") },
+                                    onValueChange = {
+                                        viewModel.usernameChange(it)
+                                    },
+                                    label = { Text(text = "Usuario") },
                                 )
 
                                 Spacer(
@@ -119,8 +145,15 @@ fun LoginFrontView(
                                 OutlinedTextField(
                                     value = dataUiState.password,
                                     onValueChange = { viewModel.passwordChange(it) },
-                                    label = { Text("Contraseña") },
-                                    visualTransformation = PasswordVisualTransformation()
+                                    label = { Text("Password") },
+                                    singleLine = true,
+                                    visualTransformation = if (dataUiState.seePass) VisualTransformation.None else PasswordVisualTransformation(),
+                                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                                    trailingIcon = {
+                                        IconButton(onClick = {viewModel.showPass()}){
+                                            Icon(imageVector  = dataUiState.iconPass, "")
+                                        }
+                                    }
                                 )
                             }
                         }
