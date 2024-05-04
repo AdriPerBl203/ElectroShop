8a818dae44d1e0dfad272cecc7515904e9cc34a1
AaronGonzalez06 <aaron060812060812@gmail.com>
2024-05-04 23:58:09 +0200

    u

--- app/src/main/java/com/AG_AP/electroshop/components/DialogListDraw.kt
+++ app/src/main/java/com/AG_AP/electroshop/components/DialogListDraw.kt
@@ -3,7 +3,6 @@ package com.AG_AP.electroshop.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.AlertDialog
@@ -12,14 +11,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
-import com.AG_AP.electroshop.uiState.MenuUiState
-import com.AG_AP.electroshop.viewModels.MenuViewModel
-import java.lang.reflect.Modifier
 
 @Composable
 fun DialogListDraw(actionItemList:()->Unit ,infoDialog:String){
@@ -52,6 +47,7 @@ fun AlertDialogExample(
         },
         text = {
             Column (
+                modifier = Modifier.fillMaxWidth(),
                 verticalArrangement= Arrangement.Center,
                 horizontalAlignment= Alignment.CenterHorizontally
             ){
