6e7985ebf8d83de54781a0d2c8f0d79de8205bc6
aperez16 <adriianperez2003@gmail.com>
2024-05-08 13:31:23 +0200

    Añadido DELETE y UPDATE DE Pedidos de Compra.

--- app/src/main/java/com/AG_AP/electroshop/data/model/LoggedInUser.kt
+++
@@ -1,9 +0,0 @@
-package com.AG_AP.electroshop.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class LoggedInUser(
-    val userId: String,
-    val displayName: String
-)
