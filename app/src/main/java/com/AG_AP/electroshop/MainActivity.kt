5ac55b1c4d9f3b87aa727d335c1d40aa5ef50773
aperez16 <adriianperez2003@gmail.com>
2024-05-24 09:22:23 +0200

    CRUD XD?.

--- app/src/main/java/com/AG_AP/electroshop/MainActivity.kt
+++ app/src/main/java/com/AG_AP/electroshop/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.AG_AP.electroshop
 
 import android.content.Context
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -20,7 +18,6 @@ import com.AG_AP.electroshop.nav.AppNav
 
 
 class MainActivity : ComponentActivity() {
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
