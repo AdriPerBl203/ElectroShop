e2aa0e697dcdef6b98cef8780668e4e5b670c4f6
aperez16 <adriianperez2003@gmail.com>
2024-06-03 17:51:06 +0200

    para ver pdf

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,4 +90,10 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("io.coil-kt:coil-gif:2.6.0")
 
+    implementation("androidx.compose.ui:ui:1.0.0")
+    implementation("androidx.compose.material:material:1.0.0")
+    implementation("androidx.compose.ui:ui-tooling:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.activity:activity-compose:1.3.0-alpha06")
+
 }
