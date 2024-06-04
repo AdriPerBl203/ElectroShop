31f1be9a8e4e62ec7e84928b01f341d5363b5302
aperez16 <adriianperez2003@gmail.com>
2024-06-04 11:51:20 +0200

    Añadido un init en el view model y cambiado la firma de una variable.

--- app/src/main/java/com/AG_AP/electroshop/uiState/InvoiceUiState.kt
+++ app/src/main/java/com/AG_AP/electroshop/uiState/InvoiceUiState.kt
@@ -4,7 +4,7 @@ import android.graphics.pdf.PdfRenderer
 import com.AG_AP.electroshop.firebase.models.InvoiceData
 
 data class InvoiceUiState(
-    val BusinessPartnerWithInvoiceList: MutableList<InvoiceData> = mutableListOf(),
+    val BusinessPartnerWithInvoiceList: MutableList<InvoiceData?> = mutableListOf(),
     val Base64String: String = "",
     val ActualPdf: PdfRenderer? = null
 )
--- app/src/main/java/com/AG_AP/electroshop/viewModels/InvoiceViewModel.kt
+++ app/src/main/java/com/AG_AP/electroshop/viewModels/InvoiceViewModel.kt
@@ -1,15 +1,50 @@
 package com.AG_AP.electroshop.viewModels
 
 import androidx.lifecycle.ViewModel
+import com.AG_AP.electroshop.firebase.InvoiceDataCRUD
+import com.AG_AP.electroshop.firebase.models.InvoiceData
 import com.AG_AP.electroshop.uiState.InvoiceUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class InvoiceViewModel : ViewModel() {
 
     private val _uiState = MutableStateFlow(InvoiceUiState())
     val uiState: StateFlow<InvoiceUiState> = _uiState.asStateFlow()
 
+    init {
+        searchData()
+    }
+
+
+    /**
+     * Method that searches for new invoice in the database and updates the list inside the uiState
+     */
+    private fun searchData() {
+        var mutableList: MutableList<InvoiceData?> = mutableListOf()
+        InvoiceDataCRUD.getAllObject {
+            mutableList = it
+
+        }
+
+        _uiState.update { currentState ->
+            currentState.copy(
+                BusinessPartnerWithInvoiceList = mutableList
+            )
+        }
+
+    }
+
+    fun replaceData(item: InvoiceData?) {
+        if (item != null) {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    //TODO
+                )
+            }
+        }
+    }
 
 }
