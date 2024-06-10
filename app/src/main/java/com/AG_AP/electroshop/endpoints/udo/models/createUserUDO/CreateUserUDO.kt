643d61ee1c04eac8d23d7a8eeca1c00368de692e
agonzalez28 <aaroooooon060812@gmail.com>
2024-06-10 10:11:27 +0200

    Cambiando nomenclatura de la tabla UDO

--- app/src/main/java/com/AG_AP/electroshop/endpoints/udo/models/createUserUDO/CreateUserUDO.kt
+++ app/src/main/java/com/AG_AP/electroshop/endpoints/udo/models/createUserUDO/CreateUserUDO.kt
@@ -2,11 +2,11 @@ package com.AG_AP.electroshop.endpoints.udo.models.createUserUDO
 
 data class CreateUserUDO(
     val Code: String,
-    val U_Empleado: Int,
-    val U_PedidoCO: String,
-    val U_PedidoCl: String,
-    val U_actividad: String,
-    val U_articulo: String,
-    val U_name: String,
-    val U_password: String
+    val U_SEIempleado: Int,
+    val U_SEIpedidoCO: String,
+    val U_SEIpedidoCl: String,
+    val U_SEIactividad: String,
+    val U_SEIarticulo: String,
+    val U_SEIname: String,
+    val U_SEIpassword: String
 )
